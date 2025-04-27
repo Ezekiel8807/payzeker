0e374fdf78895f41606ada4b815d757e6b2d9ac6
Ezekiel8807 <Ayeoluwaseyi@gmail.com>
2025-04-27 09:03:41 +0100

    fix error for deployment

--- src/components/SubmittedTaskCom.tsx
+++
@@ -1,9 +0,0 @@
-//components
-import SubmittedTaskCard from "./cards/SubmittedTaskCard";
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default function SubmittedTaskCom({ subTaskArr }: { subTaskArr: any }) {
-  return (
-
-  );
-}
