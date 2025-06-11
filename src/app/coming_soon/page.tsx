48fb48e3b33be720426007ebc07b4e80b08802fa
Ezekiel8807 <Ayeoluwaseyi@gmail.com>
2025-06-11 08:17:20 +0100

    update coming soon page

--- src/app/coming_soon/page.tsx
+++ src/app/coming_soon/page.tsx
@@ -20,10 +20,8 @@ export default function ComingSoon() {
           {/* Left Content */}
           <div className="space-y-6">
             <h1 className="text-3xl md:text-4xl font-bold">
-              Your go-to solution for <br />
-              <span className="text-[var(--green)]">
-                freelance task management
-              </span>
+              Your solution for <br />
+              <span className="text-[var(--green)]">Simple and easy tasks</span>
             </h1>
             <p className="text-gray-600">
               Payzekre helps businesses easily find reliable freelancers to
@@ -34,7 +32,7 @@ export default function ComingSoon() {
             </p>
 
             <div>
-              <p className="mb-2 text-gray-700">
+              <p className="mb-3 text-gray-700">
                 We are launching soon!!! Sign up to our newsletter to stay
                 updated
               </p>
