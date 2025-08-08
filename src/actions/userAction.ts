7fbd1701391b2b1ff8a0ad6a1722d32c80734373
Ezekiel8807 <Ayeoluwaseyi@gmail.com>
2025-08-08 17:11:45 +0100

    new update

---
+++ src/actions/userAction.ts
@@ -0,0 +1,12 @@
+// import bcrypt from "bcryptjs";
+// import { cookies } from "next/headers";
+// import User from "../../../../model/userModel";
+// import { connectDB } from "../../../../lib/mongodb";
+// import { encrypt } from "@/actions/action";
+
+// export async function Login(username: string, password: string) {
+//   //
+
+//   //connect to database
+//   await connectDB();
+// }
