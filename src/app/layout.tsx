ef989b416572b963f4a90d4e7a9dae2cd2a0cade
Ezekiel8807 <Ayeoluwaseyi@gmail.com>
2025-08-10 23:52:07 +0100

    new stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
         <meta property="og:title" content="Payzeker" />
         <meta property="og:image" content="/icons/pIcon.png" />
         <meta name="twitter:card" content="/icons/pIcon.png" />
-        <meta name="monetag" content="2c9a0e16906c03b1d0b6edae004e5a23" />
+        {/* <meta name="monetag" content="2c9a0e16906c03b1d0b6edae004e5a23" /> */}
         <meta name="google-adsense-account" content="ca-pub-3810051236937370" />
 
         <meta
@@ -51,11 +51,11 @@ export default function RootLayout({
         />
         <link rel="icon" type="image/svg+xml" href="/icons/pIcon.png" />
 
-        <Script
+        {/* <Script
           src="https://grookilteepsou.net/act/files/tag.min.js?z=9693459"
           data-cfasync="false"
           async
-        ></Script>
+        ></Script> */}
 
         <Script
           async
