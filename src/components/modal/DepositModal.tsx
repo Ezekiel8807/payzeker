aeebcf9cbdf584a59caddcc2ce0a071d8c84cb42
Ezekiel8807 <Ayeoluwaseyi@gmail.com>
2025-09-14 16:00:18 +0100

    new update

--- src/components/modal/DepositModal.tsx
+++ src/components/modal/DepositModal.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import { payWithPaystack } from "@/utils/payWithPaystack";
 
 //components
@@ -84,98 +85,93 @@ export default function DepositModal({
 
   return (
     <ModalFrame title="Deposit" closeModal={closeModal}>
-      <>
-        <form
-          onSubmit={handleDepposit}
-          className="w-[90%] md:w-[70%] my-5 mx-auto"
-        >
-          <input
-            className="w-full outline-none bg-none p-1 border-b-2 text-right"
-            type="text"
-            name="fullname"
-            readOnly
-            disabled
-            value={fullname}
-            id="fullname"
-            placeholder="Fullname"
-          />
+      <form
+        onSubmit={handleDepposit}
+        className="w-[90%] md:w-[70%] my-5 mx-auto"
+      >
+        {/* ✅ Load Paystack only on client */}
+        <Script src="https://js.paystack.co/v1/inline.js"></Script>
 
-          <div className="text-right mt-2">
-            <small className="block text-gray-400">
-              {"Balance: "}
-              {balance.toLocaleString("en-NG", {
-                style: "currency",
-                currency: "NGN",
-              })}
-            </small>
-          </div>
-
-          <input
-            className="w-full outline-none p-1 border-2 text-right"
-            type="text"
-            name="depAmount"
-            value={depAmount.toString()}
-            onChange={(e) => {
-              const notNumber = isNaN(Number(e.target.value));
-              if (notNumber) return;
-              setDepamount(Number(e.target.value));
+        {/*  */}
+        <input
+          className="w-full outline-none bg-none p-1 border-b-2 text-right"
+          type="text"
+          name="fullname"
+          readOnly
+          disabled
+          value={fullname}
+          id="fullname"
+          placeholder="Fullname"
+        />
+        <div className="text-right mt-2">
+          <small className="block text-gray-400">
+            {"Balance: "}
+            {balance.toLocaleString("en-NG", {
+              style: "currency",
+              currency: "NGN",
+            })}
+          </small>
+        </div>
+        <input
+          className="w-full outline-none p-1 border-2 text-right"
+          type="text"
+          name="depAmount"
+          value={depAmount.toString()}
+          onChange={(e) => {
+            const notNumber = isNaN(Number(e.target.value));
+            if (notNumber) return;
+            setDepamount(Number(e.target.value));
+          }}
+          placeholder={`Amount(NGN): min- #100`}
+        />
+        <div className="flex flex-row mt-2 items-center justify-between gap-2">
+          <div
+            onClick={() => {
+              setDepamount((value) => value + 100);
             }}
-            placeholder={`Amount(NGN): min- #100`}
-          />
-
-          <div className="flex flex-row mt-2 items-center justify-between gap-2">
-            <div
-              onClick={() => {
-                setDepamount((value) => value + 100);
-              }}
-              className="p-2 font-black text-[10px] text-center bg-[var(--gray-20)]"
-            >
-              +100
-            </div>
-            <div
-              onClick={() => {
-                setDepamount((value) => value + 500);
-              }}
-              className="p-2 font-black text-[10px] text-center bg-[var(--gray-20)]"
-            >
-              +500
-            </div>
-            <div
-              onClick={() => {
-                setDepamount((value) => value + 1000);
-              }}
-              className="p-2 font-black text-[10px] text-center bg-[var(--gray-20)]"
-            >
-              +1000
-            </div>
-            <div
-              onClick={() => {
-                setDepamount((value) => value + 5000);
-              }}
-              className="p-2 font-black text-[10px] text-center bg-[var(--gray-20)]"
-            >
-              +5000
-            </div>
-            <div
-              onClick={() => {
-                setDepamount((value) => value + 10000);
-              }}
-              className="p-2 font-black text-[10px] text-center bg-[var(--gray-20)]"
-            >
-              +10000
-            </div>
+            className="p-2 font-black text-[10px] text-center bg-[var(--gray-20)]"
+          >
+            +100
           </div>
-
-          <div className="text-right">
-            <Button btnStyle="w-[100px] mt-5 p-1 text-center bg-[var(--green)] rounded">
-              {isPen ? "Processing..." : "Deposit"}
-            </Button>
+          <div
+            onClick={() => {
+              setDepamount((value) => value + 500);
+            }}
+            className="p-2 font-black text-[10px] text-center bg-[var(--gray-20)]"
+          >
+            +500
           </div>
-
-          {/* ✅ Load Paystack only on client */}
-          <script src="https://js.paystack.co/v1/inline.js"></script>
-        </form>
-      </>
+          <div
+            onClick={() => {
+              setDepamount((value) => value + 1000);
+            }}
+            className="p-2 font-black text-[10px] text-center bg-[var(--gray-20)]"
+          >
+            +1000
+          </div>
+          <div
+            onClick={() => {
+              setDepamount((value) => value + 5000);
+            }}
+            className="p-2 font-black text-[10px] text-center bg-[var(--gray-20)]"
+          >
+            +5000
+          </div>
+          <div
+            onClick={() => {
+              setDepamount((value) => value + 10000);
+            }}
+            className="p-2 font-black text-[10px] text-center bg-[var(--gray-20)]"
+          >
+            +10000
+          </div>
+        </div>
+        <div className="text-right">
+          <Button btnStyle="w-[100px] mt-5 p-1 text-center bg-[var(--green)] rounded">
+            {isPen ? "Processing..." : "Deposit"}
+          </Button>
+        </div>
+      </form>
     </ModalFrame>
   );
 }
